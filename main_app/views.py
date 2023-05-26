3c70b51fe95fafb0f5b83da49c86aff0617290f2
amirb <amir.bullock9@gmail.com>
2023-05-26 12:24:53 -0400

    set up route to about page

--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,9 @@ from django.http import HttpResponse
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+
+def about(request):
+  return render(request, 'about.html')
+
+def cat_index(request):
+  return render(request, 'cats/index.html', { 'cats': cats })
