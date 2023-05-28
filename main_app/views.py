1503f03b99526baa967b82f2b6acf0614317eb23
amirb <amir.bullock9@gmail.com>
2023-05-28 15:20:37 -0400

    update route and link to finches list, add model for Finch and migrations

--- main_app/views.py
+++ main_app/views.py
@@ -1,26 +1,12 @@
 from django.shortcuts import render
-
-from django.http import HttpResponse
-
-class Finch:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-finches = [
-  Finch('Lolo', 'tabby', 'Kinda rude.', 3),
-  Finch('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-  Finch('Fancy', 'bombay', 'Happy fluff ball.', 4),
-  Finch('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
+from .models import Finch
 
 def home(request):
-  return HttpResponse('<h1>Hello tweet tweet </h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def finch_index(request):
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html', { 'finches': finches })
