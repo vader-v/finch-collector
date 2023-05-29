8af0b7b34f5374770052085eac18cc6bf304b6a6
amirb <amir.bullock9@gmail.com>
2023-05-29 16:13:33 -0400

    add forms.py and feeding form to detail.html

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
+from .forms import FeedingForm
 
 def home(request):
   return render(request, 'home.html')
@@ -14,7 +15,11 @@ def finch_index(request):
 
 def finch_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
-  return render(request, 'finches/detail.html', { 'finch': finch })
+  feeding_form = FeedingForm()
+  return render(request, 'finches/detail.html', {
+    'finch': finch,
+    'feeding_form': feeding_form
+  })
 
 class FinchCreate(CreateView):
   model = Finch
