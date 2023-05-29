0f4e09b8c47ee97f85eaf25607a66b44c726bd56
amirb <amir.bullock9@gmail.com>
2023-05-29 10:14:35 -0400

    add Feedings model and MEALS tuple

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner'),
+)
+
 class Finch(models.Model):
   name = models.CharField(max_length=100)
   breed = models.CharField(max_length=100)
@@ -10,4 +16,13 @@ class Finch(models.Model):
     return self.name
   def get_absolute_url(self):
       return reverse("finch-detail", kwargs={'finch_id' : self.id})
-  
+
+class Feeding(models.Model):
+  date = models.DateField()
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
