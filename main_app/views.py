df2ff2a5df2486ce1b68efddee612790929a9eb2
amirb <amir.bullock9@gmail.com>
2023-05-29 16:55:28 -0400

    add mcdp for feeding and views function for feedings

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 from .forms import FeedingForm
@@ -20,6 +20,14 @@ def finch_detail(request, finch_id):
     'finch': finch,
     'feeding_form': feeding_form
   })
+
+def add_feeding(request, finch_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('finch-detail', finch_id=finch_id)
 
 class FinchCreate(CreateView):
   model = Finch
