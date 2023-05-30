f3fa86faa1fb9c9b83cf9745a35c8dd3cb0297dd
amirb <amir.bullock9@gmail.com>
2023-05-30 19:45:32 -0400

    update model indentation, and migrate accordingly

--- main_app/models.py
+++ main_app/models.py
@@ -31,5 +31,5 @@ class Feeding(models.Model):
   def __str__(self):
     return f"{self.get_meal_display()} on {self.date}"
 
-class Meta:
-  ordering = ['-date']
+  class Meta:
+    ordering = ['-date']
