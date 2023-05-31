48869c0ccdab7c7ad315a36a0221ee4e01f55c1f
amirb <amir.bullock9@gmail.com>
2023-05-30 20:03:48 -0400

    create fed_for_today model method and import date

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 MEALS = (
   ('B', 'Breakfast'),
@@ -18,6 +19,9 @@ class Finch(models.Model):
 
   def get_absolute_url(self):
       return reverse("finch-detail", kwargs={'finch_id': self.id})
+  
+  def fed_for_today(self):
+    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 
 class Feeding(models.Model):
   date = models.DateField('Feeding date')
