0e226d7d4803bf3f278627f7c9581195dadc6bf7
amirb <amir.bullock9@gmail.com>
2023-06-01 17:23:02 -0400

    update path and view function for home

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.views import LoginView
 from .models import Finch, Toy
 from .forms import FeedingForm
 
 
-def home(request):
-  return render(request, 'home.html')
+class Home(LoginView):
+  template_name = 'home.html'
 
 def about(request):
   return render(request, 'about.html')
