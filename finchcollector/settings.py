687bc8cf18ff7c93856b97d1118b87df3298e160
amirb <amir.bullock9@gmail.com>
2023-06-01 17:40:33 -0400

    add signup template, fix url redirect

--- finchcollector/settings.py
+++ finchcollector/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-LOGIN_REDIRECT_URL = 'cat-index'
+LOGIN_REDIRECT_URL = 'finch-index'
 LOGOUT_REDIRECT_URL = 'home'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
