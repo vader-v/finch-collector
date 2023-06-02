dea4cadc023ec0d86734c67d2be0918f25ef1d40
amirb <amir.bullock9@gmail.com>
2023-06-02 10:53:42 -0400

    complete finchcollector deployment

--- finchcollector/settings.py
+++ finchcollector/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
+# adjust these lines
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'amirfinchcollector.fly.dev']
+CSRF_TRUSTED_ORIGINS = ['https://amirfinchcollector.fly.dev']
 # Application definition
 
 INSTALLED_APPS = [
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
 	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -52,6 +54,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# add these lines
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'finchcollector.urls'
 
